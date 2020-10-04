86fdbf9e2006fddc769bcba88e929ab6f3e2fd4f
alexsoulisweb <alexsoulisdev@outlook.com>
2020-10-04 13:54:51 +0200

    Project
    Comming to project

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xsltcmtkdj86o1c=rjq5hmny-tdev_eyyq+bokoyc=sh*z95m='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS=["shopsoulis.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
